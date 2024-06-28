03364b36da8f4de223d70b795b21d1656b9c28dc
Ritu <ritu.basak01@gmail.com>
2024-06-28 19:05:19 +0530

    28-6-24 -- processing

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper(".mySwiper", {
     direction: "vertical",
-    speed: 2000,
+    speed: 1000,
     slidesPerView: 1,
     mousewheel: true,
     pagination: {
@@ -14,9 +14,9 @@ var swiper = new Swiper(".mySwiper", {
 // Custom text for each pagination bullet
 var bulletTexts = [
     '<i class="fa-solid fa-house nav-ico"></i> <span class="nav-text">Home</span>',
-    '<i class="fa-solid fa-house nav-ico"></i> <span class="nav-text">Home</span>',
-    '<i class="fa-solid fa-house nav-ico"></i> <span class="nav-text">Home</span>',
-    '<i class="fa-solid fa-house nav-ico"></i> <span class="nav-text">Home</span>'
+    '<i class="fa-solid fa-user nav-ico"></i> <span class="nav-text">About Us</span>',
+    '<i class="fa-solid fa-book nav-ico"></i> <span class="nav-text">Training</span>',
+    '<i class="fa-solid fa-location-dot nav-ico"></i> <span class="nav-text">Contact</span>'
 ];
 
 // Ensure bullets are updated on initial load
@@ -33,7 +33,7 @@ swiper.emit('paginationUpdate');
 var aniSwiper = new Swiper(".aniSwiper", {
     speed: 2000,
     slidesPerView: 1,
-    mousewheel: true,
+    // mousewheel: true,
     // pagination: {
     //     el: ".ani-swiper-pagination",
     //     clickable: true,
@@ -50,49 +50,21 @@ $(document).ready(function () {
     });
 });
 
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     const customCursor = document.getElementById('custom-cursor');
-//     document.addEventListener('mousemove', (e) => {
-//         customCursor.style.left = `${e.clientX}px`;
-//         customCursor.style.top = `${e.clientY}px`;
-//         // alert(`${e.clientY}px`);
-//         console.log(`${e.clientX}px`);
-//         console.log(`${e.clientY}px`);
-//     });
-   
-// });
-
-
+// let scrolledAfterTransition = false;
+// let scrollDirection;
 // window.addEventListener('wheel', function(event) {
+//     scrollDirection = event.deltaY > 0 ? 'down' : 'up';
 //     if (swiper.activeIndex === 1) {
 //         swiper.mousewheel.disable();
-
-//         // Check if aniSwiper is at both the beginning and the end (meaning it has only one slide)
-       
-//         if (aniSwiper.isEnd || aniSwiper.isBeginning) {
-//             alert('hi');
+//     }
+//     scrolledAfterTransition = true; 
+// });
+// aniSwiper.on('transitionEnd', function() {
+//     if ((aniSwiper.isEnd && scrollDirection === 'down') || (aniSwiper.isBeginning && scrollDirection === 'up')) {
+//         if (scrolledAfterTransition) {
 //             swiper.mousewheel.enable();
 //         }
 //     }
+//     scrolledAfterTransition = false; 
 // });
-
-
-let scrolledAfterTransition = false;
-let scrollDirection;
-window.addEventListener('wheel', function(event) {
-    scrollDirection = event.deltaY > 0 ? 'down' : 'up';
-    if (swiper.activeIndex === 1) {
-        swiper.mousewheel.disable();
-    }
-    scrolledAfterTransition = true; 
-});
-aniSwiper.on('transitionEnd', function() {
-    if ((aniSwiper.isEnd && scrollDirection === 'down') || (aniSwiper.isBeginning && scrollDirection === 'up')) {
-        if (scrolledAfterTransition) {
-            swiper.mousewheel.enable();
-        }
-    }
-    scrolledAfterTransition = false; 
-});
 
